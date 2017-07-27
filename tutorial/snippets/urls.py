151010ea83d975b34f54709383835fe21536a890
Lukasz Siczek <lukasz.siczek@stxnext.pl>
2017-07-27 20:51:42 +0200

    Schema

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -9,7 +9,15 @@ router.register(r'users', views.UserViewSet)
 
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
+
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
+
+
+
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
